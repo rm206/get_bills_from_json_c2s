9a0f6758fff20c2fbad26c0c515b25a8bb7c774d
Rishabh <panicpark60@gmail.com>
2024-05-14 19:03:07 -0500

    stuff

--- eval_set.py
+++ eval_set.py
@@ -1,41 +1,64 @@
 import pandas as pd
 import requests
 import json
-from bs4 import BeautifulSoup
 from pprint import pprint
 
 df = pd.read_csv("politifact_claims.csv")
 
+# cos similarity models -
+# msmarco-MiniLM-L-6-v3
+# msmarco-MiniLM-L-12-v3
+# msmarco-distilbert-base-v3
+# msmarco-distilbert-base-v4
+# msmarco-roberta-base-v3
 
-for i, claim in enumerate(df["claim_entered"]):
-    url = "https://idir.uta.edu/claimlens/submit?query=" + claim
+models = [
+    "msmarco-MiniLM-L-6-v3",
+    "msmarco-MiniLM-L-12-v3",
+    "msmarco-distilbert-base-v3",
+    "msmarco-distilbert-base-v4",
+    "msmarco-roberta-base-v3",
+]
 
-    response = requests.get(url, timeout=20).text
-    soup = BeautifulSoup(response, "html.parser")
-    all_titles = soup.find_all("li", class_="results-column-description")
-    bill_titles = []
-    for title in all_titles:
-        if title.text.split(":")[0] == "Bill Title":
-            bill_titles.append(title.text.split(":")[1].strip())
+for model in models:
+    for i, claim in enumerate(df["claim_entered"]):
+        url = "http://127.0.0.1:8000/api?claim=" + claim + "&model_name=" + model
 
-    if bill_titles == []:
-        df.loc[i, "bills_found"] = "no_bill_found"
-    else:
-        df.loc[i, "bills_found"] = str(bill_titles)
+        bill_titles = []
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            df.loc[i, model] = str(response.status_code) + "_error"
+            continue
+
+        with open("response.json", "w") as file:
+            json.dump(response.json(), file)
+        with open("response.json", "r") as file:
+            data = json.load(file)
+
+        if not data["bills"]:
+            df.loc[i, model] = "no_bill_found"
+            continue
+
+        for entity in data["bills"]:
+            bill_titles.append(entity["bill_title"])
+
+        df.loc[i, model] = str(bill_titles)
+
 
 df.to_csv("politifact_claims.csv", index=False)
 
-# from bs4 import BeautifulSoup
-# from pprint import pprint
 
-# with open("response.html", "r") as file:
-#     data = file.read()
-
-# soup = BeautifulSoup(data, "html.parser")
-# all_titles = soup.find_all("li", class_="results-column-description")
-# bill_titles = []
-# for title in all_titles:
-#     if title.text.split(":")[0] == "Bill Title":
-#         bill_titles.append(title.text.split(":")[1].strip())
-
-# pprint(bill_titles)
+"""
+soup = BeautifulSoup(response, "html.parser")
+all_titles = soup.find_all("li", class_="results-column-description")
+bill_titles = []
+for title in all_titles:
+    if title.text.split(":")[0] == "Bill Title":
+        bill_titles.append(title.text.split(":")[1].strip())
+        if bill_titles == []:
+            df.loc[i, "bills_found"] = "no_bill_found"
+        else:
+            df.loc[i, "bills_found"] = str(bill_titles)
+"""
--- overlap_calc.py
+++ overlap_calc.py
@@ -2,15 +2,32 @@ import pandas as pd
 
 df = pd.read_csv("politifact_claims.csv")
 
-for i, bills in enumerate(df["sourced_bills"]):
-    if df.loc[i, "bills_found"] == "no_bill_found":
-        df.loc[i, "overlap"] = "no_bill_found"
-        continue
+models = [
+    "msmarco-MiniLM-L-6-v3",
+    "msmarco-MiniLM-L-12-v3",
+    "msmarco-distilbert-base-v3",
+    "msmarco-distilbert-base-v4",
+    "msmarco-roberta-base-v3",
+]
 
-    sourced_bills = eval(bills)
-    bills_found = eval(df.loc[i, "bills_found"])
+scores = {}
+for model in models:
+    scores[model] = 0
 
-    overlap = len(set(sourced_bills) & set(bills_found))
-    df.loc[i, "overlap"] = f"{overlap} of {len(sourced_bills)}"
+for model in models:
+    cnt = 0
+    sum_frac = 0
 
-df.to_csv("politifact_claims.csv", index=False)
+    for i, bills in enumerate(df["sourced_bills"]):
+        if df.loc[i, model] == "no_bill_found":
+            continue
+
+        sourced_bills = eval(bills)
+        bills_found = eval(df.loc[i, model])
+        overlap = len(set(sourced_bills) & set(bills_found))
+        sum_frac += overlap / len(sourced_bills)
+        cnt += 1
+
+    scores[model] = sum_frac / cnt
+
+print(scores)
